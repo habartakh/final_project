e1f70d19a9c63700d6225dd1ae1688223b09092d
habartakh <bartakh.hajar@gmail.com>
2025-08-21 13:44:06 +0000

    Separated nodes into different launch files

--- moveit2_scripts/launch/pick_and_place.launch.py
+++ moveit2_scripts/launch/pick_and_place.launch.py
@@ -4,7 +4,9 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
 
 def generate_launch_description():
 
@@ -21,10 +23,27 @@ def generate_launch_description():
         output="screen", 
     )
 
+    moveit_config = MoveItConfigsBuilder("name", package_name="custom_moveit_config").to_moveit_configs()
+
+    # MoveItCpp demo executable
+    moveit_cpp_node = Node(
+        name="pick_and_place",
+        package="moveit2_scripts",
+        executable="pick_and_place",
+        output="screen",
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            {'use_sim_time': True},
+        ],
+    )
+
 
     return LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(move_rviz_file_path)
         ),
         add_objects_planning_scene_node,
+        moveit_cpp_node,
     ])
